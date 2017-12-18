31a4a0005e62a0df9fa12966782cb2ba281765ca
carlosjcaro <carlosjesuscaro@icloud.com>
2017-12-18 05:07:23 -0500

    classifier 27

--- superciao_regex.py
+++ superciao_regex.py
@@ -57,7 +57,7 @@ def regex_filter(data, uuid):
 	counter = 0
 
 	for row in data:
-		regex_test = re.findall(r'(\b|\n)(\w{0,}standort\w{0,}|\W{0,}Stätte\W{0,})(\b|\n)', row, re.IGNORECASE)
+		regex_test = re.findall(r'(\b|\n)(gerichtsstand|gesetz\w{0,}|Vertragsrecht|Kaufrecht)(\b|\n)', row, re.IGNORECASE)
 		if regex_test:
 			regex_index.append(uuid[counter])
 			regex_clause.append(row)
